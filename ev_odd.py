a2a0de00fc1b16cb8d9b04acd3cf9a57d95536fe
Mageshwaran1997 <40879496+Mageshwaran1997@users.noreply.github.com>
2018-07-07 09:39:24 +0530

    Update ev_odd.py

--- ev_odd.py
+++ ev_odd.py
@@ -1,11 +1,7 @@
 a= raw_input()
-print(a)
-print type (a)
-print "Converting the input as integer"
 b=int(a)
-print type (b)
 if(b%2==0):
-	st= "The given value is even"
+	st= "even"
 else:
-	st="The given number is odd"
+	st="odd"
 print(st)
